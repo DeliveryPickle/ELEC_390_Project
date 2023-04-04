a84ac87b60296fa3eaf053096c2a75a5dc165c63
Elise <elise.quail@queensu.ca>
2023-04-04 13:53:43 -0400

    SMA and NOrmalization

--- PreProcessing.py
+++ PreProcessing.py
@@ -19,37 +19,86 @@ def data_split(data):
         i = j
     return outList
 
-matrix1 = pd.read_csv("Raw Data.csv")
-matrix2 = pd.read_csv("JumpingBackPocket.csv")
-temp = data_split(matrix1)
-dataset1 = temp[0]
-temp = data_split(matrix2)
-dataset2 = temp[0]
+# Preprocessing
+filePath = ["WalkingFrontPocket.csv", "WalkingBackPocket.csv", "WalkingCoatPocket.csv", "WalkingArmsUp.csv", "WalkingArmsDown.csv", "JumpingFrontPocket.csv", "JumpingBackPocket.csv", "JumpingCoatPocket.csv", "JumpingArmsUp.csv", "JumpingArmsDown.csv"]
+Elise_matrix = {}
+Simon_matrix = {}
+Lucas_matrix = {}
 
 window_size = 71
-dataset1_sma = dataset1.rolling(window_size).mean()
-dataset2_sma = dataset2.rolling(window_size).mean()
-fig, ax = plt.subplots(2, 2)
-dataset1.plot(x = "Time (s)",ax=ax.flatten()[0:5],subplots=True, sharex=False)
-fig, ax = plt.subplots(2, 2)
-dataset2.plot(x = "Time (s)",ax=ax.flatten()[0:5],subplots=True, sharex=False)
-fig, ax = plt.subplots(2, 2)
-dataset1_sma.plot(x = "Time (s)",ax=ax.flatten()[0:5],subplots=True, sharex=False)
-fig, ax = plt.subplots(2, 2)
-dataset2_sma.plot(x = "Time (s)",ax=ax.flatten()[0:5],subplots=True, sharex=False)
 
+for i in filePath:
+    Elise_matrix = pd.read_csv("Elise/" + i)
+    temp = data_split(Elise_matrix)
+    for j in range(len(temp)):
+        j = temp[j]
+        if len(j) < window_size:
+            # fig, ax = plt.subplots(2, 2)
+            # j.plot(x="Time (s)", ax=ax.flatten()[0:5], subplots=True, sharex=False)
+            # fig.suptitle('Elise' + str(i) + '(Unfiltered)', fontsize=12)
 
+            # Moving Average Filter
+            Elise_data_sma = j.rolling(window_size).mean()
+            # fig, ax = plt.subplots(2, 2)
+            # Elise_data_sma.plot(x="Time (s)", ax=ax.flatten()[0:5], subplots=True, sharex=False)
+            # fig.suptitle('Elise' + str(i) + '(Unfiltered)', fontsize=12)
 
-# fig, axs = plt.subplots(2, 2)
-# axs[0, 0].plot(dataset)
-# axs[0, 1].plot(dataset_sma5)
-# axs[1, 0].plot(dataset_sma30)
-# axs[1, 1].plot(dataset_sma50)
-# plt.ylabel('Y')
-# plt.xlabel('X')
-# plt.plot(dataset)
-# plt.plot(dataset_sma5)
-# plt.plot(dataset_sma30)
-# plt.plot(dataset_sma50)
+            # Normalization
+            Elise_data_normalized = Elise_data_sma.copy()
+            column = 'Absolute acceleration (m/s^2)'
+            Elise_data_normalized[column] = (Elise_data_normalized[column] - Elise_data_normalized[column].min()) / (Elise_data_normalized[column].max() - Elise_data_normalized[column].min())
+            # fig, ax = plt.subplots(2, 2)
+            # Elise_data_normalized.plot(x="Time (s)", ax=ax.flatten()[0:5], subplots=True, sharex=False)
+            # fig.suptitle('Normalized Elise ' + str(i) + ' (SMA 71)', fontsize=12)
+            # plt.show()
 
-plt.show()
+for i in filePath:
+    Simon_matrix = pd.read_csv("Simon/" + i)
+    temp = data_split(Simon_matrix)
+    for j in range(len(temp)):
+        j = temp[j]
+        if len(j) > window_size:
+            # fig, ax = plt.subplots(2, 2)
+            # j.plot(x="Time (s)", ax=ax.flatten()[0:5], subplots=True, sharex=False)
+            # fig.suptitle('Simon' + str(i) + '(Unfiltered)', fontsize=12)
+
+            # Moving Average Filter
+            Simon_data_sma = j.rolling(window_size).mean()
+            # fig, ax = plt.subplots(2, 2)
+            # Simon_data_sma.plot(x="Time (s)", ax=ax.flatten()[0:5], subplots=True, sharex=False)
+            # fig.suptitle('Simon' + str(i) + '(Unfiltered)', fontsize=12)
+
+            # Normalization
+            Simon_data_normalized = Simon_data_sma.copy()
+            column = 'Absolute acceleration (m/s^2)'
+            Simon_data_normalized[column] = (Simon_data_normalized[column] - Simon_data_normalized[column].min()) / (Simon_data_normalized[column].max() - Simon_data_normalized[column].min())
+            # fig, ax = plt.subplots(2, 2)
+            # Simon_data_normalized.plot(x="Time (s)", ax=ax.flatten()[0:5], subplots=True, sharex=False)
+            # fig.suptitle('Normalized Simon ' + str(i) + ' (SMA 71)', fontsize=12)
+            # plt.show()
+
+for i in filePath:
+    Lucas_matrix = pd.read_csv("Lucas/" + i)
+    temp = data_split(Lucas_matrix)
+    for j in range(len(temp)):
+        j = temp[j]
+        if len(j) > window_size:
+            # fig, ax = plt.subplots(2, 2)
+            # j.plot(x="Time (s)", ax=ax.flatten()[0:5], subplots=True, sharex=False)
+            # fig.suptitle('Lucas' + str(i) + '(Unfiltered)', fontsize=12)
+
+            # Moving Average Filter
+            Lucas_data_sma = j.rolling(window_size).mean()
+            # fig, ax = plt.subplots(2, 2)
+            # Lucas_data_sma.plot(x="Time (s)", ax=ax.flatten()[0:5], subplots=True, sharex=False)
+            # fig.suptitle('Lucas' + str(i) + '(Unfiltered)', fontsize=12)
+
+            # Normalization
+            Lucas_data_normalized = Lucas_data_sma.copy()
+            column = 'Absolute acceleration (m/s^2)'
+            Lucas_data_normalized[column] = (Lucas_data_normalized[column] - Lucas_data_normalized[column].min()) / (Lucas_data_normalized[column].max() - Lucas_data_normalized[column].min())
+            # fig, ax = plt.subplots(2, 2)
+            # Lucas_data_normalized.plot(x="Time (s)", ax=ax.flatten()[0:5], subplots=True, sharex=False)
+            # fig.suptitle('Normalized Lucas ' + str(i) + ' (SMA 71)', fontsize=12)
+            plt.show()
+
